c627c9b0dde7e85f5a8849c9b04fcc8c08da888e
Jisan-Hasan <jisan.jh5@gmail.com>
2022-09-02 22:23:19 +0600

    static news card design

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ const loadCategories = () => {
 const displayCategories = categories => {
     const categoriesContainer = document.getElementById('categories-container');
     categories.forEach(category => {
-        console.log(category);
+        // console.log(category);
         const categoryDiv = document.createElement('div');
         categoryDiv.innerHTML = `
             ${category.category_name}
@@ -24,3 +24,32 @@ const displayCategories = categories => {
 
 
 loadCategories();
+
+
+
+// Load News based on Category
+
+
+
+
+
+// Test Section
+// const testImg = () => {
+    
+//     fetch('https://openapi.programming-hero.com/api/news/category/01')
+//         .then(res => res.json())
+//         .then(data => displayImg(data.data[0]))
+
+// }
+
+// const displayImg = data => {
+//     const imgSection = document.getElementById('test');
+//     console.log(data)
+//     imgSection.innerHTML = `
+//             <img  src="${data.author.img}" alt="">
+//         `;
+    
+// }
+
+
+// testImg();
